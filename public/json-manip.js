1ba4ffbd4ae0ff688a5198f3f4b8122740fe8621
weathertopper <nathanweatherly94@gmail.com>
2017-12-31 15:05:23 -0500

    reinstalled npm packages and added some json manip stubs for later when my brain is working

--- public/json-manip.js
+++ public/json-manip.js
@@ -1,8 +1,15 @@
 //  returns JSON
 /**
- * 
- * 
+ * - turn transaction type into "+" or "-" (for credit or debit) based on amount
+ * - fill in all daily posted balance slots using amount, transaction type, and existing daily bosted balances
+ * - add catagory column to every row
  */
 const trimJSON = (csv_as_json) => {
+    console.log(csv_as_json);
+    return csv_as_json;
+}
+
+// returns json
+const updateCatagories = (csv_as_json) => {
     return csv_as_json;
 }
